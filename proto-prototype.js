5cb37effb2a836dbe58ce46801db8f41a4d68e90
Viacheslav Bartosh <231globus@gmail.com>
2022-12-01 21:40:10 +0300

    Update proto-prototype.js

--- proto-prototype.js
+++ proto-prototype.js
@@ -1,4 +1,33 @@
-let val1 = {};
-let val2 = {};
+// let obj1 = {};
+// let obj2 = {};
+// console.log(obj1.__proto__ === obj2.__proto__); 
 
-console.log(val1.__proto__ === val2.__proto__);
+// let str = "Kyky";
+// let int =  15;
+// console.log(str.__proto__ === int.__proto__);
+
+// function f1() {}
+// class c1 {}
+// console.log(f1.__proto__ === c1.__proto__);
+
+///////////////////////////////////////////////////////
+
+// let obj = {}
+// console.log(obj.__proto__ === Object.prototype)
+
+// class Person {}
+// console.log(Person.__proto__ === Function.prototype)
+
+// let vasya = new Person();
+// console.log(vasya.__proto__ === Person.prototype)
+
+///////////////////////////////////////////////////////
+
+// let worker = { name: "vasya" }
+// console.log(worker.prototype);
+
+//////////////////////////////////////////////////////////
+
+// let Person = (name) => {return name}
+// console.log(Person.prototype); //false, протоипа нету у стрелочных фкункция
+// console.log(Person.__proto__); // есть ли прото и чем он равен? (Есть и прото равно function.prototype)
